bcd2700f20b32094c90fac373f69c01294fa99dc
dks3 <grommolniu@yandex.ru>
2013-10-29 02:35:37 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Astat::Application.config.secret_key_base = '7b2f86c775c67e2e38ac17c94c20a9fdfd999b534bec9e8fd0ad6a22337ce1b072582c78592a75b70a026615de93d1c613a6f4eb764bb1c57ceec56115074cef'

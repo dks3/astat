2dcbf5078c2b1931baaac44f883453e5ae11e426
Kirill Dushkevich <dks3@mail.msiu.ru>
2013-12-23 02:59:45 +0400

    stat

--- app/models/subdivision.rb
+++ app/models/subdivision.rb
@@ -6,6 +6,10 @@ class Subdivision < ActiveRecord::Base
 
 def calls
   numbers=people.includes(:city).all.map{|p| p.city.try(:number)}.compact
-  Cdr.where(src: numbers.map{|x| "495#{x}"})
+  puts "-----------------------------------------------"
+  puts numbers.inspect
+  puts "-----------------------------------------------"
+  #Cdr.where(src: numbers.map{|x| "495#{x}"})
+  Cdr.where("src in (?) or dst in (?)",numbers.map{|x| "495#{x}"},numbers.map{|x| "495#{x}"}).order("billsec")
 end
 end
